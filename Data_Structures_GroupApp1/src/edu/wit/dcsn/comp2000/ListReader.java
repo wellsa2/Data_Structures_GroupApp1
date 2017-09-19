7311a4f543d1f7a177f9ba5431d680a790b0dda2
horowitzb <horowitzb@WITPC0EF3SM.wit.private>
2017-09-19 14:20:39 -0400

    more javadoc

--- Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/ListReader.java
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/ListReader.java
@@ -1,84 +1,80 @@
 package edu.wit.dcsn.comp2000;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * class ListReader has an arraylist storing multiple objects of type GroceryItem with a default constructor to fill it with info from an input string
- * that corresponds to a text file
- * has a get method for the whole arraylist
- * has a get method for individual arraylists
- * has a toString method
+ * class ListReader uses an arraylist of type groceryItem, with a constructor
+ * for inputting a filename, and a default constructor with a default file. Has
+ * methods for returning the arraylist or individual groceryItems.
+ * 
  * @author horowitzb
  */
-public class ListReader 
-	{
-	
-	//instance variables
+public class ListReader {
+
+	// instance variables
 	/**
 	 * resizeable array (arraylist) holding type GroceryItem
 	 */
 	private ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>(20);
-	
-	//constructors
-	
+
+	// constructors
+
 	/**
 	 * default constructor. uses groceries-1.txt as input string / filename
 	 */
-	public ListReader() 
-	{
+	public ListReader() {
 		this("groceries-1.txt");
 	}
-	
+
 	/**
-	 * constructor fills groceryList with objects of type GroceryItem from a provided text file 
+	 * constructor fills groceryList with objects of type GroceryItem from a
+	 * provided text file
+	 * 
 	 * @param GroceriesTextFile
 	 */
-	public ListReader(String GroceriesTextFile)
-		{
-		try
-			{
+	public ListReader(String GroceriesTextFile) {
+		try {
 			Scanner sc = new Scanner(new FileReader(GroceriesTextFile));
-			while(sc.hasNext())
-				{
+			while (sc.hasNext()) {
 				groceryList.add(new GroceryItem(sc.next(), sc.next(), sc.next(), sc.next(), sc.next()));
-				}
-			} 
-		catch (FileNotFoundException e)
-			{
+			}
+		} catch (FileNotFoundException e) {
 			System.out.println("File " + GroceriesTextFile + " not found");
-			}	
-		}//end ListReader constructor	
-	
-	//public methods
-	
+		}
+	}// end ListReader constructor
+
+	// public methods
+
 	/**
 	 * returns the full GroceryList
+	 * 
 	 * @return groceryList
 	 */
-	public ArrayList<GroceryItem> getGroceryList(){
+	public ArrayList<GroceryItem> getGroceryList() {
 		return groceryList;
-	}//end getGroceryList
-	
+	}// end getGroceryList
+
 	/**
-	 *returns GroceryItem at location i in the array
+	 * returns GroceryItem at location i in the array
 	 *
-	 * @param int i
+	 * @param int i from the range 0 to groceryList.size() - 1
 	 * @return groceryItem
 	 */
 	public GroceryItem getGroceryItem(int i) {
 		return groceryList.get(i);
-	}//end getGroceryItem
-	
+	}// end getGroceryItem
+
 	/**
 	 * returns a string containing a toString of all GroceryItem objects
 	 */
 	public String toString() {
 		StringBuilder listString = new StringBuilder();
-		for(int i = 0; i < groceryList.size(); i++) {
+		for (int i = 0; i < groceryList.size(); i++) {
 			listString.append(groceryList.get(i).toString());
 		}
 		return listString.toString();
 	}
-	}// end ListReader class
+}// end ListReader class
