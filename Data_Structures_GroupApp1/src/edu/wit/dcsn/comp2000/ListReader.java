ef7d859b8f1d0c6639fd1618652622181d7e8357
horowitzb <horowitzb@WITPC0EF3SM.wit.private>
2017-09-19 13:56:55 -0400

    more javadoc

--- Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/ListReader.java
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/ListReader.java
@@ -3,11 +3,27 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+/**
+ * class ListReader has an arraylist storing multiple objects of type GroceryItem with a default constructor to fill it with info from an input string
+ * that corresponds to a text file
+ * has a get method for the whole arraylist
+ * has a get method for individual arraylists
+ * has a toString method
+ * @author horowitzb
+ */
 public class ListReader 
 	{
 	
+	/**
+	 * resizeable array (arraylist) holding type GroceryItem
+	 */
 	private ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>(20);
 	
+	/**
+	 * constructor fills groceryList with objects of type GroceryItem from a provided text file 
+	 * @param GroceriesTextFile
+	 */
 	public ListReader(String GroceriesTextFile)
 		{
 		try
@@ -25,7 +41,7 @@ public class ListReader
 		}//end ListReader constructor	
 	
 	/**
-	 * 
+	 * returns the full GroceryList
 	 * @return groceryList
 	 */
 	public ArrayList<GroceryItem> getGroceryList(){
@@ -33,14 +49,18 @@ public class ListReader
 	}//end getGroceryList
 	
 	/**
+	 *returns GroceryItem at location i in the array
 	 *
 	 * @param int i
-	 * @return groceryItem at int i
+	 * @return groceryItem
 	 */
 	public GroceryItem getGroceryItem(int i) {
 		return groceryList.get(i);
 	}//end getGroceryItem
 	
+	/**
+	 * returns a string containing a toString of all GroceryItem objects
+	 */
 	public String toString() {
 		StringBuilder listString = new StringBuilder();
 		for(int i = 0; i < groceryList.size(); i++) {
