257414a88ae0479af53e60b2ab0999b103f69aca
Aiden Wells <wellsa1@wit.edu>
2017-09-19 06:29:19 -0400

    grocery text file addded and list reaer format adjusted.

--- Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/GroceryItem.java
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/GroceryItem.java
@@ -10,23 +10,29 @@ package edu.wit.dcsn.comp2000;
  */
 //TODO documentation
 public class GroceryItem {
+	final private String NAME;
 	final private String SIZE;
 	final private String WEIGHT;
 	final private String FIRMNESS;
 	final private boolean FRAGILE;
 	
-	
-	public GroceryItem(String size, String weight, String firmness, boolean fragile)
+	public GroceryItem(String name, String size, String weight, String firmness, String breakable)
 	{
+		this.NAME = name;
 		this.SIZE=size;
 		this.WEIGHT=weight;
 		this.FIRMNESS=firmness;
-		this.FRAGILE=fragile;
+		this.FRAGILE=(breakable.equals("breakable"))? true : false;
 	}
 
+	public String getName() {
+		return NAME;
+	}
+	
 	public String getSize()
 	{
 		return SIZE;
+
 	}
 	
 	public String getWeight()
---
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/ListReader.java
@@ -0,0 +1,26 @@
+package edu.wit.dcsn.comp2000;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ListReader 
+	{
+	
+	private ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>(20);
+	
+	public ListReader(String GroceriesTextFile)
+		{
+		try
+			{
+			Scanner sc = new Scanner(new FileReader(GroceriesTextFile));
+			while(sc.hasNext())
+				{
+				groceryList.add(new GroceryItem(sc.next(), sc.next(), sc.next(), sc.next(), sc.next()));
+				}
+			} 
+		catch (FileNotFoundException e)
+			{
+			System.out.println("File " + GroceriesTextFile + " not found");
+			}	
+		}//end ListReader constructor		
+	}// end ListReader class
