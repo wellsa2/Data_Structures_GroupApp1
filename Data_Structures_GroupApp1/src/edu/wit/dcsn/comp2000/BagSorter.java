90ede1b0cfdf4ce8144945ec93637523fa1b2ed5
leslievongphakdy <vongphakdyl@wit.edu>
2017-09-22 14:40:59 -0400

    Update BagSorter.java

--- Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/BagSorter.java
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/BagSorter.java
@@ -5,26 +5,21 @@ public class BagSorter extends GroceryBag {
 	//TODO all logic
 	/* 
 	* make a new (empty) bag and fill it with items that corresponds
-	* to each pair of Weight+Firmness. Max of 10 items a bag. The program should
+	* to each pair of firmness. Max of 12 items a bag. The program should
 	* traverse through groceries-1.txt to
 	*/
 	private GroceryItem[] items; //array to store groceries
 	
 	private int numItems; //# of item per bag
 	
-	public static final int DEFAULT_MAX_SIZE = 10; //declares max amount
+	public static final int DEFAULT_MAX_SIZE = 12; //declares max amount
 	
 	//default sized, empty bag
-	public Bag(){
+	public BagA(){
 		items = new GroceryItem[DEFAULT_MAX_SIZE];
 		numItems = 0;
 	}
 	
-	//TODO: add item from list that contains the words soft and light 
-	public boolean add(GroceryItem item){
-	
-		
-	}
 	
 	/* TODO:
 	* make new bags that will take whatever the first bag didn't.
