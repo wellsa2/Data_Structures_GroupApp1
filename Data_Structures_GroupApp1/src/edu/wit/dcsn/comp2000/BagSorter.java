2fa3d5d860da9b743e97fc24b78b0c3eacaccabf
leslievongphakdy <vongphakdyl@wit.edu>
2017-09-24 15:37:06 -0400

    Update BagSorter.java

--- Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/BagSorter.java
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/BagSorter.java
@@ -2,39 +2,99 @@ package edu.wit.dcsn.comp2000;
 
 
 public class BagSorter  {
-	//TODO all logic
 	/* 
 	* if a bag is full create a new bag
-	* use as many bag as needed
+	* use 3 bags with max of 9 items
 	*/
 	private GroceryItem[] items; //array to store groceries
-	private int numItems; //# of item per bag
+	private int n; //# of item per bag
 	
-	public static final int DEFAULT_MAX_SIZE = 6; //declares max amount
+	public static final int DEFAULT_MAX_SIZE = 9; //declares max amount
 	
 	//default sized, empty bag
 	public Bag(){
 		items = new GroceryItem[DEFAULT_MAX_SIZE];
-		numItems = 0;
+		n = 0;
 	}
+	//adds items and returns false if bag is full   
+	public boolean add(GroceryItem item) {
+		if (item == null)
+			throw new IllegalArgumentException("item cannot be null");     
+		if (n == items.length)
+			return false;
+		else 
+		{
+			items[n] = item;    
+			n++;
+        
+			return true;
+		}
+	}
+	
 	//is it empty?
 	public boolean isEmpty(){
-		return == 0;
+		return items == 0;
+	}
+	//is it full?
+	public boolean isFull() {
+		return (n == items.length);
 	}
 	
-	public boolean removeItems(Bag twoBag){
-        // check input
-        if (twoBag == null || twoBag.numItems() == 0)
-            return false;
-	//
+	public twoBag(){
+		items = new GroceryItem[DEFAULT_MAX_SIZE];
+		n = 0;
+	}
+	    
+	public boolean add(GroceryItem item) {
+		if (item == null)
+			throw new IllegalArgumentException("item cannot be null");     
+		if (n == items.length)
+			return false;
+		else 
+		{
+			items[n] = item;    
+			n++;
+        
+			return true;
+		}
+	}
 	
+	//is it empty?
+	public boolean isEmpty(){
+		return items == 0;
+	}
+	//is it full?
+	public boolean isFull() {
+		return (n == items.length);
+	}
 	
+	public threeBag(){
+		items = new GroceryItem[DEFAULT_MAX_SIZE];
+		n = 0;
+	}
+	    
+	public boolean add(GroceryItem item) {
+		if (item == null)
+			throw new IllegalArgumentException("item cannot be null");     
+		if (n == items.length)
+			return false;
+		else 
+		{
+			items[n] = item;    
+			n++;
+        
+			return true;
+		}
+	}
 	
-	/* TODO:6
-	* make new bags that will take whatever the first bag didn't.
-	* then fill this bag and see if there are items left
-	*/
-	
+	//is it empty?
+	public boolean isEmpty(){
+		return items == 0;
+	}
+	//is it full?
+	public boolean isFull() {
+		return (n == items.length);
+	}
 					
 }
 
