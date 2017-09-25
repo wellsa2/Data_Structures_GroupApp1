ac33ad77971048df534e0aa4106094cfca063c53
Avery Loftin <loftina@wit.edu>
2017-09-25 11:20:26 -0400

    fixed main

--- Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/Main.java
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/Main.java
@@ -1,11 +1,7 @@
 package edu.wit.dcsn.comp2000;
 
-<<<<<<< HEAD
 public class Main 
 {
-=======
-public class Main {
->>>>>>> branch 'master' of https://github.com/wellsa2/Data_Structures_GroupApp1.git
 	public static void main(String[] args)
 	{
 		BagSorter bagSorter = new BagSorter();
