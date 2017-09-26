8390f4796e88b1ad08101af156cbb58d4a6d8399
leslievongphakdy <vongphakdyl@wit.edu>
2017-09-25 22:54:31 -0400

    Update ItemWeight.java

--- Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/ItemWeight.java
+++ Data_Structures_GroupApp1/src/edu/wit/dcsn/comp2000/ItemWeight.java
@@ -80,11 +80,60 @@ public enum ItemWeight
     } // end toString
     
     
-    /**
+	/**
 	 * Unit Tester for ItemWeight
 	 */
 	public static void main(String[] args)
 	{
-		
+		System.out.println( "\n----------\nTesting ItemWeight:\n" );
+		testNameAndValue();
+		testMethods();
 	} // end main
+	
+	
+	/**
+	 * tester for display names and values
+	 */
+	private static void testNameAndValue()
+	{
+		System.out.println( "\n----------\nTesting Names and Values:\n" );
+		
+		printTest(true, "Testing LIGHT display name", ItemWeight.LIGHT.displayName, "Light");
+		printTest(true, "Testing LIGHT value", String.valueOf(ItemWeight.LIGHT.weightValue), "1");
+		
+		printTest(true, "Testing MEDIUM display name",ItemWeight .MEDIUM.displayName, "Medium");
+		printTest(true, "Testing MEDIUM value", String.valueOf(ItemWeight.MEDIUM.weightValue), "2");
+
+		printTest(true, "Testing HEAVY display name", ItemWeight.HEAVY.displayName, "Heavy");
+		printTest(true, "Testing HEAVY value", String.valueOf(ItemWeight.HEAVY.weightValue), "3");
+	} // end testNameAndValue()
+	
+	
+	/**
+	 * tester for interpretDescription and toString
+	 */
+	private static void testMethods()
+	{
+		System.out.println( "\n----------\nTesting Methods:\n" );
+		
+		printTest(true, "Testing interpretDescription with valid string", interpretDescription("medium").displayName, "Medium");
+		printTest(false, "Testing interpretDescription with undefined string", interpretDescription("z").displayName, "Medium"); // Medium is default value
+		printTest(false, "Testing interpretDescription with null", String.valueOf(interpretDescription(null)), "null");
+		
+		printTest(true, "Testing toString", weightValue.Heavy.toString(), "Heavy");
+		
+	} // end testMethods
+	
+	
+	/**
+	 * utility function to print out testing info
+	 * @param isValid  is this testing valid parameters or invalid ones
+	 * @param description  a description of the test being run
+	 * @param recieved  output received by the test
+	 * @param expected  what the received output should be
+	 */
+	private static void printTest(boolean isValid, String description, String recieved, String expected)
+	{
+		System.out.println(String.format("Is Valid: %s%nDescription: %s%nRecieved: %s%nExpected: %s%n", isValid, description, recieved, expected));
+	} // end printTest
 } // end ItemWeight
